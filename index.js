3dfe02aaa0897f3a199944976658ff345ae3c4c8
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-10-27 13:03:56 +0600

    added data

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ app.use(cors());
 const course = require('./course.json');
 
 app.get('/', (req, res) => {
-    res.send("News API is running Now!");
+    res.send("course API is running Now!");
 })
 app.get('/course', (req, res) => {
     res.send(course)
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedcourse = course.find(n => n.course_id === id);
+    res.send(selectedcourse);
 })
 app.listen(5000)
